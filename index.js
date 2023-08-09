63491f9ffab62fb730e88d0f6dad023149c956f7
Ali <42673799+justoali@users.noreply.github.com>
2023-08-08 22:54:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Losiento,si hice algo que te molestara😔, y si prodia hacer algo para que ya no estes molesta extraño a mi dueña♡')
+});
+
+const noBtn = document.querySelector('#noBtn')
+
+noBtn.addEventListener('click',function () {
+    alert('si no estas molesta por que ya no hablamos,Ya son 4 dias😔 estraño a mi dueña♡')
+});
